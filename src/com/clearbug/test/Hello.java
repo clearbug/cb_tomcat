9805bc72df6cdcf65e341008f9bb7ce53665d755
clearbug <craftsman_gao@qq.com>
2017-04-30 12:24:21 +0800

    first commit

---
+++ src/com/clearbug/test/Hello.java
@@ -0,0 +1,11 @@
+package com.clearbug.test;
+
+/**
+ * Created by clearbug on 2017/4/30.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
