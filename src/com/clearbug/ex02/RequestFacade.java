d07d8ecfeeeb589f95028396c8fb0b8f16dbe296
clearbug <craftsman_gao@qq.com>
2017-05-01 18:46:57 +0800

    使用简单的门面设计模式改进简单的 Servlet 容器

--- src/com/clearbug/ex02/RequestFacade.java
+++ src/com/clearbug/ex02/RequestFacade.java
@@ -5,61 +5,23 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.Date;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
 
 /**
- * Created by clearbug on 2017/4/30.
+ * Created by clearbug on 2017/5/1.
  */
-public class Request implements ServletRequest {
+public class RequestFacade implements ServletRequest {
 
-    private InputStream input;
-    private String uri;
+    private ServletRequest request = null;
 
-    public Request(InputStream input) {
-        this.input = input;
+    public RequestFacade(Request request) {
+        this.request = request;
     }
 
-    public String getUri() {
-        return uri;
-    }
-
-    private String parseUri(String requestString) {
-        int index1, index2;
-        index1 = requestString.indexOf(' ');
-        if (index1 != -1) {
-            index2 = requestString.indexOf(' ', index1 + 1);
-            if (index2 > index1)
-                return requestString.substring(index1 + 1, index2);
-        }
-        return null;
-    }
-
-    public void parse() {
-        // Read a set of characters from the socket
-        StringBuffer request = new StringBuffer(2048);
-        int i;
-        byte[] buffer = new byte[2048];
-        try {
-            System.out.println("----------------------------Socket read data start on: " + (new Date()));
-            i = input.read(buffer);  // 这里直接一次性读取 2048 个字节的数据的逻辑上应该是有问题的。首先，请求的数据可能不止 2048 个字节；其次，输入流可能不会一次性读出所有的请求数据。这里这么写应该是基于请求很简单的原因。
-            System.out.println("----------------------------Socket read data end on: " + (new Date()) + ", the length of data is " + i);
-        } catch (IOException e) {
-            e.printStackTrace();
-            i = -1;
-        }
-        for (int j = 0; j < i; j++) {
-            request.append((char) buffer[j]);
-        }
-        System.out.print(request.toString());
-        this.uri = parseUri(request.toString());
-    }
-
-    /* implementation of ServletRequest */
+    /* implementation of the ServletRequest */
     @Override
     public Object getAttribute(String s) {
         return null;
