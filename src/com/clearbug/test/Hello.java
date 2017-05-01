d3541899d2393ca0a945737b8b11a02c5ae801a9
clearbug <craftsman_gao@qq.com>
2017-05-01 15:47:03 +0800

    Test git in IDEA

--- src/com/clearbug/test/Hello.java
+++ src/com/clearbug/test/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
         System.out.println(System.getProperty("user.dir"));
+        System.out.println("Test git in IDEA.");
     }
 }
